9ac54c62ace000f70901a8cece81777e64df02b4
Ehren Coker <mrcoker@gmail.com>
2015-12-17 16:33:31 -0600

    Add Map

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import npmResolve from 'resolve';
 import bowerResolve from 'resolve-bower';
 
 let options = {};
+let _map = [];
 
 function find(resolver, file) {
 
@@ -30,23 +31,25 @@ find.bower = function(file) {
  * Look for Sass files installed through npm
  * @return {Function}         Function to be used by node-sass importer
  */
-export default function(opts, map) {
+export default function(opts) {
 
   options = opts || options;
-  map = map || [];
+  _map = arguments[1] || [];
 
   const aliases = new Map();
 
   return function(url, _, done) {
 
     if (aliases.has(url)) {
-      map.push(aliases.get(url));
+
+      _map.push(aliases.get(url));
       return done({ file: aliases.get(url) });
 
     }
 
     find.npm(url).then(find.bower).then((file) => {
-      map.push(file);
+
+      _map.push(file);
       aliases.set(url, file);
       done({ file });
 
